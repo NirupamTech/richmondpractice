a4dd4305b0f11e4d04e2e82c503b56bf7b4957c4
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-17 20:05:54 +0530

    Update formulas.js

--- formulas.js
+++ formulas.js
@@ -1,48 +1,43 @@
-const formulasData = {
-  math: {
-    formulas: `
-      Math Formulas:
-      Formula 1: ...
-      Formula 2: ...
-    `,
-    topics: 'Math Topics: Topic 1, Topic 2, ...'
-  },
-  physics: {
-    formulas: `
-      Physics Formulas:
-      Formula 1: ...
-      Formula 2: ...
-    `,
-    topics: 'Physics Topics: Topic 1, Topic 2, ...'
-  },
-  chemistry: {
-    formulas: `
-      Chemistry Formulas:
-      Formula 1: ...
-      Formula 2: ...
-    `,
-    topics: 'Chemistry Topics: Topic 1, Topic 2, ...'
-  },
-  bio: {
-    formulas: `
-      Biology Formulas:
-      Formula 1: ...
-      Formula 2: ...
-    `,
-    topics: 'Biology Topics: Topic 1, Topic 2, ...'
-  }
-};
+// Define the formulas for each subject
+const mathFormulas = `
+Math Formulas:
+Formula 1: ...
+Formula 2: ...
+`;
 
+const physicsFormulas = `
+Physics Formulas:
+Formula 1: ...
+Formula 2: ...
+`;
+
+const chemistryFormulas = `
+Chemistry Formulas:
+Formula 1: ...
+Formula 2: ...
+`;
+
+const bioFormulas = `
+Biology Formulas:
+Formula 1: ...
+Formula 2: ...
+`;
+
+// Function to get formulas based on subject
 function getFormulasBySubject(subject) {
-  return formulasData[subject] ? formulasData[subject].formulas : 'Formulas for this subject are not available.';
+  switch (subject) {
+    case 'math':
+      return mathFormulas;
+    case 'physics':
+      return physicsFormulas;
+    case 'chemistry':
+      return chemistryFormulas;
+    case 'bio':
+      return bioFormulas;
+    default:
+      return 'Formulas for this subject are not available.';
+  }
 }
 
-function getTopicsBySubject(subject) {
-  return formulasData[subject] ? formulasData[subject].topics : 'Topics for this subject are not available.';
-}
-
-// Export the functions
-module.exports = {
-  getFormulasBySubject,
-  getTopicsBySubject
-};
+// Make the function accessible globally
+window.getFormulasBySubject = getFormulasBySubject;
