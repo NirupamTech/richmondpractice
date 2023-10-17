9d542b54c532e9b9bda8615c37b55a09f5f3189c
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-17 20:14:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,27 +10,28 @@ function generateTimetable() {
     Difficulty: ${difficulty}
     Total Study Time: ${studyTime} minutes
 
-    Important Topics:
-    ${getTopicsBySubject(subject)}
-
     Formulas:
     ${formulasContent}
   `;
 
-  // Create a new window to print the content
   const printWindow = window.open('', '_blank');
   printWindow.document.write('<html><head><title>Study Timetable</title></head><body>');
   printWindow.document.write('<pre>' + content + '</pre>');
   printWindow.document.write('</body></html>');
   printWindow.document.close();
 
-  // Print the window
-  printWindow.print();
+  // Wait for content to load before printing
+  printWindow.onload = function() {
+    printWindow.print();
+    printWindow.onafterprint = function() {
+      printWindow.close();
+    };
+  };
 }
 
-// Function to get topics based on subject
-function getTopicsBySubject(subject) {
-  // Add logic to get topics based on subject
-  // This is just a placeholder
-  return 'Topics for this subject:\nTopic 1: ...\nTopic 2: ...';
+// Rest of your code for getting formulas from formulas.js
+
+// Function to get formulas based on subject
+function getFormulasBySubject(subject) {
+  return getFormulasBySubject(subject);
 }
