21ae03e02df6020b20f01e6fe9dffb507c8e1887
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-17 20:06:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ function generateTimetable() {
   const studyTime = parseInt(document.getElementById('studyTime').value);
 
   const formulasContent = getFormulasBySubject(subject);
-  const topicsContent = getTopicsBySubject(subject);
 
   const content = `
     Subject: ${subject}
@@ -12,33 +11,26 @@ function generateTimetable() {
     Total Study Time: ${studyTime} minutes
 
     Important Topics:
-    ${topicsContent}
+    ${getTopicsBySubject(subject)}
 
     Formulas:
     ${formulasContent}
   `;
 
+  // Create a new window to print the content
   const printWindow = window.open('', '_blank');
   printWindow.document.write('<html><head><title>Study Timetable</title></head><body>');
   printWindow.document.write('<pre>' + content + '</pre>');
   printWindow.document.write('</body></html>');
   printWindow.document.close();
 
+  // Print the window
   printWindow.print();
-}
-
-// Rest of your code for getting formulas and topics from formulas.js
-
-// Function to get formulas based on subject
-function getFormulasBySubject(subject) {
-  // Add logic to get formulas based on subject
-  // This is just a placeholder
-  return 'Formulas for this subject are not available.';
 }
 
 // Function to get topics based on subject
 function getTopicsBySubject(subject) {
   // Add logic to get topics based on subject
   // This is just a placeholder
-  return 'Topics for this subject are not available.';
+  return 'Topics for this subject:\nTopic 1: ...\nTopic 2: ...';
 }
