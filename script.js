c9c552ccfd0b736c5d8614a20891cdd2eab88893
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-17 21:23:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+// Import the formulas from formulas.js
+import { mathFormulas, physicsFormulas, chemistryFormulas, bioFormulas } from './formulas.js';
+
+// Function to generate the timetable
 function generateTimetable() {
   const subject = document.getElementById('subject').value;
   const difficulty = document.getElementById('difficulty').value;
   const studyTime = parseInt(document.getElementById('studyTime').value);
+
+  // Function to get formulas based on subject
+  function getFormulasBySubject(subject) {
+    switch (subject) {
+      case 'math':
+        return mathFormulas;
+      case 'physics':
+        return physicsFormulas;
+      case 'chemistry':
+        return chemistryFormulas;
+      case 'bio':
+        return bioFormulas;
+      default:
+        return 'Formulas for this subject are not available.';
+    }
+  }
 
   // Fetch the formulas for the selected subject
   const formulasContent = getFormulasBySubject(subject);
@@ -26,34 +46,19 @@ function generateTimetable() {
     </html>
   `;
 
-  const blob = new Blob([htmlContent], { type: 'text/html' });
-  const url = URL.createObjectURL(blob);
+  const printWindow = window.open('', '_blank');
+  printWindow.document.write(htmlContent);
+  printWindow.document.close();
 
-  const printWindow = window.open(url, '_blank');
-  printWindow.onload = function() {
+  // Wait for content to load before printing
+  printWindow.onload = function () {
     printWindow.print();
-    printWindow.onafterprint = function() {
-      URL.revokeObjectURL(url);
+    printWindow.onafterprint = function () {
       printWindow.close();
     };
   };
 }
 
-// Rest of your code for getting formulas from formulas.js
-
-// Function to get formulas based on subject
-function getFormulasBySubject(subject) {
-  // Replace with actual logic to fetch formulas from formulas.js
-  switch (subject) {
-    case 'math':
-      return mathFormulas;
-    case 'physics':
-      return physicsFormulas;
-    case 'chemistry':
-      return chemistryFormulas;
-    case 'bio':
-      return bioFormulas;
-    default:
-      return 'Formulas for this subject are not available.';
-  }
-}
+// Add an event listener to the button for generating the timetable
+const generateButton = document.getElementById('generateButton');
+generateButton.addEventListener('click', generateTimetable);
