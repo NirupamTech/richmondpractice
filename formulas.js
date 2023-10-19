4d1ea755ecdd20d043ccfd9c1439860999737751
Nirupam <72504541+NirupamTech@users.noreply.github.com>
2023-10-19 13:11:46 +0530

    Update formulas.js

--- formulas.js
+++ formulas.js
@@ -267,12 +267,349 @@ Limits and Derivatives:
 f(a-0)=limx→a− f(x) = limh→0f(a-h)
 
 Similarly, right hand limit is given as,
-f(a+0)=\lim_{x\to a^+}f(x)=\lim_{h\to 0}f(a+h)
+f(a+0)=limx→a+ f(x) = limh→0 f(a+h)
 
-A limit \lim_{x\to a}f(x)        exists when:
-\lim_{x\to a^-}f(x)        and \lim_{x\to a^+}f(x)        both exists or,
+A limit limx→a f(x) exists when:
+limx→a− f(x) and limx→a+ f(x) both exists or,
 
-\lim_{x\to a^-}f(x) = \lim_{x\to a^+}f(x)
+limx→a− f(x) = limx→a+ f(x)
+
+Some Important Properties of Limits: Consider f and g be two functions such that both limx→a f(x) and limx→a g(x) exists, then:
+limx→a [f(x) ± g(x)] = limx→a f(x) ± limx→a g(x)
+limx→a kf(x) = k*{limx→a f(x)}
+limx→a f(x)*g(x) = limx→a f(x) * limx→a g(x)
+limx→a {f(x)/g(x)} = {limx→a f(x)/limx→a g(x)}
+
+Some Standard Limits are given as:
+limx→a {x^n - a^n}/x-a = n(a)^n-1
+limx→0 sinx/x = 1
+limx→0 tanx/x = 1
+limx→0 {a^x - 1}/x = ln(a) {ln(a) means log(a) to the base "e"}
+limx→0 {e^x - 1}/x = 1
+limx→0 log(1+x)/x = 1
+
+Derivatives: Consider a real-valued function f, such that:
+f'(x) = limh→0 {f(x+h)-f(x)}/h
+is known as the Derivative of function f at x if and only if,
+limh→0 {f(x+h)-f(x)}/h exists finitely.
+
+Some Important Properties of Derivatives: Consider f and g be two functions such that their derivatives can be defined in a common domain as:
+d/dx[f(x)+g(x)] = d/dx[f(x)] + d/dx[g(x)]
+d/dx[f(x)-g(x)] = d/dx[f(x)] - d/dx[g(x)]
+d/dx[f(x)*g(x)] = d/dx[f(x)]*g(x) + d/dx[g(x)]*f(x)
+d/dx[f(x)/g(x)] = {d/dx[f(x)]*g(x) - d/dx[g(x)]*f(x)}/{[g(x)]^2}
+
+Some Standard Derivatives are given as:
+d/d(x^n) = n(x)^n-1
+d/dx(sinx) = cosx
+d/dx(cosx) = -sinx
+d/dx(tanx) = sec^2x
+d/dx(secx) = secx*tanx
+d/dx(cotx) = -csc^2x
+d/dx(e^x) = e^x
+d/dx(a^x) = (a^x)(loga)
+d/dx(logx) = 1/x
+d/dx(cscx) = -cscx*cotx
+
+Mathematical Reasoning:
+Statements: A statement is a sentence which either true or false, but not both simultaneously. For example: “A triangle has four sides.”, “New Delhi is the capital of India.” are the statements.
+Negation of a statement: Negation of a statement p: If p denote a statement, then the negation of p is denoted by ∼p.
+Compound statement: A statement is a compound statement if it is made up of two or more smaller statements. The smaller statements are called component statements of the compound statement. The Compound statements are made by:
+Connectives: “AND”, “OR”
+Quantifiers: “there exists”, “For every”
+Implications: The meaning of implications “If ”, “only if ”, “ if and only if ”.
+“p ⇒ q” : 
+p is a sufficient condition for q or p implies q.
+q is necessary to condition for p. The converse of a statement p ⇒ q is the statement q ⇒ p.
+p⇒ q together with its converse gives p if and only if q.
+“p ⇔ q”:
+p implies q (denoted by p ⇒ q)
+p is a sufficient condition for q
+q is a necessary condition for p
+p only if q
+∼q implies ∼p
+Contrapositive: The contrapositive of a statement p ⇒ q is the statement ∼ q ⇒∼p.
+Contradiction: If to check whether p is true we assume negation p is true.
+Validating statements: Checking of a statement whether it is true or false. The validity of a statement depends upon which of the special. The following methods are used to check the validity of statements:
+direct method
+contrapositive method
+method of contradiction
+using a counterexample.
+
+Probability:
+Probability = Number of Favourable outcomes in an Event / Total number of Outcomes
+Event: An event is a subset of the S (sample space). An empty set is also known as the Impossible event.
+For any random experiment, let S be the sample space. The probability P is a real-valued function whose domain is the power set of S and [0, 1] is the range interval. For any event E: P(E) ≥ 0 and P(S) = 1
+Mutually exclusive events: If E and F are mutually exclusive events, then: P(E ∪ F) = P(E) + P(F)
+Equally likely outcomes: All outcomes with equal probability are called equally likely outcomes. Let S be a finite sample space with equally likely outcomes and A be the event. Therefore, the probability of an event A is: P(A) = n(A) / n(S), where n(A) is the number of elements on the set A and n(S) is the Total number of outcomes or the number of elements in the sample space S
+Let P and Q be any two events, then the following formulas can be derived.
+Event P or Q: The set P ∪ Q
+Event P and Q: The set P ∩ Q
+Event P and not Q: The set P – Q
+P and Q are mutually exclusive if P ∩ Q = φ
+Events P1, P2, . . . . . , Pn are exhaustive and mutually exclusive if P1 ∪ P2 ∪ . . . . . ∪ Pn = S and Ei ∩ Ej = φ for all i ≠ j.
+
+Inverse Trigonometric Functions:
+Inverse trigonometric functions: Inverse trigonometric functions map real numbers back to angles. e.g. Inverse of sine function denoted by sin-1 or arc sin(x) is defined on [-1,1].
+
+Some of the important useful properties of Inverse Trigonometric Functions are:
+
+Functions     Domain	        Range
+y = sin⁻¹x	  [–1, 1]	        [−π/2,π/2]
+y = cos⁻¹x	  [–1, 1]	        [0,π]
+y = cosec⁻¹x	R – (–1, 1)	    [−π/2, π/2] – {0}
+y = sec⁻¹x	  R – (–1, 1)	    [0,π] – {π/2, π/2}
+y = tan⁻¹x	  R               (−π/2, π/2)
+y = cot⁻¹x	  R               (0,π)
+
+
+Self-Adjusting Trigonometric Property
+Self Adjusting Inverse Trigonometric Properties are,
+
+sin(sin⁻¹x)=x
+sin⁻¹(sin x) = x
+cos(cos⁻¹x) = x
+cos⁻¹(cos x) = x
+tan(tan⁻¹x) = x
+tan⁻¹(tan x) = x
+sec(sec⁻¹x) = x
+sec⁻¹(sec x) = x
+cosec⁻¹(cosec x) = x
+cosec(cosec⁻¹x) = x
+cot⁻¹(cot x) = x
+cot(cot⁻¹x) = x 
+
+Reciprocal Relations
+Reciprocal Relations of the Inverse Trigonometric Relations are,
+
+sin⁻¹(1/x) = cosec⁻¹x, x ≥ 1 or x ≤ -1
+cos⁻¹(1/x) = sec⁻¹x, x ≥ 1 or x ≤ -1
+tan⁻¹(1/x) = cot⁻¹x, x > 0
+Even and Odd Functions
+Even and Odd Functions of the Inverse Trigonometry Functions are,
+
+sin⁻¹(-x) = -sin⁻¹(x), x ∈ [-1, 1]
+tan⁻¹(-x) = -tan⁻¹(x), x ∈ R
+cosec⁻¹x(-x) = -cosec⁻¹x(x), |x| ≥1
+cos⁻¹(-x) = π – cos⁻¹(x), x ∈ [-1, 1]
+sec⁻¹(-x) = π – sec⁻¹(x), |x| ≥1 
+cot⁻¹(-x) = π – cot⁻¹(x), x ∈ R
+
+Complementary Relations
+The complementary relation of the Inverse trigonometry functions is,
+sin⁻¹x + cos⁻¹x = π/2
+tan⁻¹x + cot⁻¹x = π/2
+cosec⁻¹x + sec⁻¹x = π/2
+
+Sum and Difference Formulae
+The sum and difference formulas are the important formulas used in inverse trigonometric functions, some of the important inverse trigonometric sum and difference formulas are,
+
+tan⁻¹x + tan⁻¹y = tan⁻¹{(x+y)/(1−xy)}
+tan⁻¹x – tan⁻¹y = tan⁻¹{(x-y)/(1+xy)}
+sin⁻¹x  + sin⁻¹y = sin⁻¹[x√(1-y^2)+y√(1-x^2)]
+sin⁻¹x – sin⁻¹y = sin⁻¹[x√(1-y^2)-y√(1-x^2)]
+cos⁻¹x + cos⁻¹y = cos⁻¹[xy-√(1-x^2)√(1-y^2)]
+cos⁻¹x – cos⁻¹y = cos⁻¹[xy+√(1-x^2)√(1-y^2)]
+cot⁻¹x + cot⁻¹y = cot⁻¹[(xy-1)/(x+y)]
+cot⁻¹x + cot⁻¹y = cot⁻¹[(xy+1)/(y-x)]
+
+Double Angle Formula
+The double angle formula for the inverse trigonometric functions is,
+
+2tan⁻¹x = sin⁻¹(2x/1+x^2)
+2tan⁻¹x = cos⁻¹(1-x^2/1+x^2)
+2tan⁻¹x = tan⁻¹(2x/1-x^2)
+2sin⁻¹x = sin⁻¹(2x√(1+x^2))
+2cos⁻¹x = sin⁻¹(2x√(1-x^2))
+
+Conversion Properties
+The conversion properties for the inverse trigonometric function are,
+
+sin⁻¹x = cos⁻¹(√(1-x^2) = tan⁻¹{x/x√(1-x^2)} = cot⁻¹{√(1-x^2)/x}
+cos⁻¹x = sin⁻¹(√(1-x^2) = tan⁻¹{√(1-x^2)/x} = cot⁻¹{x/√(1-x^2)}
+tan⁻¹x = sin⁻¹{x/√(1-x^2)} = cos⁻¹{x/√(1+x^2)} = sec⁻¹√(1+x^2) = cosec⁻¹{√(1+x^2)/x}
+
+Matrices:
+Matrix: A matrix is said to have an ordered rectangular array of functions or numbers. A matrix of order m × n consists of m rows and n columns.
+Order of Matrix: If a matrix has m rows and n columns, then its order is written as m × n. If a matrix has order m × n, then it has mn elements
+Square Matrix: An m × n matrix will be known as a square matrix when m = n.
+Diagonal Matrix: A = [aij]m × m will be known as a diagonal matrix if aij = 0, when i ≠ j.
+Scalar Matrix: A = [aij]n × n is a scalar matrix if aij = 0, when i ≠ j, aij = k, (where k is some constant); and i = j.
+Identity Matrix: A = [aij]n × n is an identity matrix, if aij = 1, when i = j and aij = 0, when i ≠ j.
+Zero Matrix: A zero matrix will contain all its elements as zero.
+Column Matrix: A matrix which is of the form [A]n×1 is called the column matrix.
+Row Matrix: A matrix that is of the form [A]1×n is called the row matrix.
+A = [aij] = [bij] = B if and only if:
+A and B are of the same order
+aij = bij for all the certain values of i and j
+Operations on Matrices: Between two or more two matrices, the following operations are defined below:
+Addition of Matrix: If A = [aij]m×n and B = [yij]m×n, then A + B = [aij +bij]m×n, 1 ≤ i ≤ m, 1 ≤ j ≤ n
+Subtraction of Matrix: If A = [aij]m×n and B = [bij]m×n, then A – B = [aij – bij]m×n, 1 ≤ i ≤ m, 1 ≤ j ≤ n
+Multiplication of a matrix by scalar number: Let A = [aij]m×n be a matrix and k is scalar, then kA is another matrix obtained by multiplying each element of A by the scalar k, i.e. if A = [aij]m×n, then kA = [kaij]m×n
+Multiplication of Matrices: Let A and B be two matrices. Then, their product AB is defined, if the number of columns in matrix A is equal to the number of rows in matrix B.
+
+Determinants:
+Determinant of a Matrix
+The determinant is the numerical value of the square matrix. So, to every square matrix A = [aij] of order n, we can associate a number (real or complex) called the determinant of the square matrix A. It is denoted by det A or |A|. 
+
+e.g. The determinant of a matrix A = [a11]1 × 1 can be given as: |a11| = a11.
+
+Before finding the Determinant of a Matrix we must learn the Minor and Cofactor of a Matrix.
+
+Minor
+Minor of an element ay of a determinant is a determinant obtained by deleting the ith row and jth column in which element ay lies. Minor of an element aij is denoted by Mij.
+
+Note: Minor of an element of a determinant of order n(n ≥ 2) is a determinant of order (n – 1).
+
+Cofactor
+Cofactor of an element aij of a determinant, denoted by Aij or Cij is defined as Aij = (-1)i+j Mij, where Mij is a minor of an element aij.
+
+Value of a Determinant
+Value of determinant of a matrix of order 2, 
+A = |a11 a12|
+    |a21 a22|
+    
+|A| = a11*a22 - a12*a21
+
+Value of determinant of a matrix of order 3,
+A = |a11 a12 a13|
+    |a21 a22 a23|
+    |a31 a32 a33|
+
+|A| = a11(a22*a33 - a23*a32) - a12(a21*a33 - a23*a31) + a13(a21*a32 - a22*a31)
+
+Singular and Non-Singular Matrix
+If the value of the determinant corresponding to a square matrix is zero, then the matrix is said to be a singular matrix, otherwise it is a non-singular matrix, i.e. for a square matrix A, if |A| ≠ 0, then it is said to be a non-singular matrix and of |A| = 0, then it is said to be a singular matrix.
+
+Determinant Theorems
+If A and B are non-singular matrices of the same order, then AB and BA are also non-singular matrices of the same order.
+The determinant of the product of matrices is equal to the product of their respective determinants, i.e. |AB| = |A||B|, where A and B are a square matrix of the same order.
+Adjoint of a Matrix
+The adjoint of a square matrix ‘A’ is the transpose of the matrix which is obtained by cofactors of each element of a determinant corresponding to that given matrix. It is denoted by adj(A).
+
+In general, the adjoint of a matrix A = [aij]n×n is a matrix [Aji]n×n, where Aji is a cofactor of element aji.
+
+Properties of Adjoint of a Matrix
+
+If A is a square matrix of order n × n, then
+
+A(adj A) = (adj A)A = |A| In
+|adj A| = |A|n-1
+adj (AT) = (adj A)T
+
+Inverse of a Square Matrix
+Let A be a non-singular matrix such that |A| ≠ 0 then the inverse of the matrix is defined as
+
+A⁻¹ = 1/|A|*adj(A)
+
+Properties of an Inverse Matrix
+
+(A⁻¹)⁻¹ = A
+(A^T)⁻¹=(A⁻¹)^T
+(AB)⁻¹ = B⁻¹A⁻¹
+(ABC)⁻¹ = C⁻¹B⁻¹A⁻¹
+adj (A⁻¹) = (adj A)⁻¹
+
+Integrals:
+Integration is the inverse process of differentiation. In differential calculus, we are given a function and we have to find the derivative or differential of this function, but in integral calculus, we are to find a function whose differential is given. Thus, integration is a process that is the inverse of differentiation.
+
+Then, ∫f(x) dx = F(x) + C, these integrals are called indefinite integrals or general integrals. C is an arbitrary constant by varying which one gets different anti-derivatives of the given function.
+
+Derivative of a function is unique but a function can have infinite anti-derivatives or integrals.
+
+Properties of Indefinite Integral
+Various properties of the integral are,
+
+∫[f(x) + g(x)] dx = ∫f(x) dx + ∫g(x) dx
+For any real number k, ∫k f(x) dx = k∫f(x)dx.
+
+In general, if f1, f2,………, fn are functions and k1, k2,…, kn are real numbers, then 
+
+∫[k1f1(x) + k2 f2(x)+…+ knfn(x)] dx = k1 ∫f1(x) dx + k2 ∫ f2(x) dx+…+ kn ∫fn(x) dx
+First Fundamental Theorem of Integral Calculus
+Let the area function be defined as
+
+A(x) = ∫axf(x)dx for all x ≥ a
+
+where the function f is assumed to be continuous on [a, b]
+
+Then A’ (x) = f (x) for every x ∈ [a, b].
+
+Second Fundamental Theorem of Integral Calculus
+Let f be the certain continuous function of x defined on the closed interval [a, b] then,
+
+∫f(x)dx = [F(x) + C]ba = F(b)−F(a) {limits are from a to b}
+
+Learn more about, Fundamental Theorem of Calculus
+
+Standard Integrals Formulas
+Standard Integral Formulas are,
+
+∫x^ndx = x^n+1/(n+1) + C        (where, n ≠ −1)
+∫cos x dx = sin x + C
+∫sin x dx = −cos x + C
+∫sec2x dx = tan x + C
+∫cosec2x dx = −cot x + C
+∫sec x.tan x dx = sec x + C
+∫cosec x.cot x dx = −cosec x + C
+∫e^xdx = ex + C
+∫a^xdx = axlogea + C
+∫1/x dx = log|x| + C
+
+Other Integral Formulas
+Other integral formulas that are widely used are,
+
+∫tan x dx = log|sec x| + C
+∫cot x dx = log|sin x| + C
+∫sec x dx =log|sec x + tan x| + C
+∫cosec x dx = log|cosec x − cot x| + C
+
+Applications of Integrals:
+This chapter included topics like how to find the area of different geometrical figures such as circles, parabolas, and ellipses.  The area enclosed by the curve y = f (x); x-axis and the lines x = a and x = b (b > a) is given by the formula,
+
+Area = ∫y.dx  =∫f(x).dx {limits are from a to b}
+Area of the region bounded by the curve x = φ (y) as its y-axis and the lines y = c, y = d is given by the formula:
+
+Area = ∫x.dy = ∫ϕ(y).dy {limits are from c to d}
+The area enclosed in between the two given curves y = f (x), y = g (x), and the lines x = a, x = b is given by the following formula:
+
+Area = ∫[f(x)−g(x)].dx, {limits are from a to b} {where, f(x) ≥ g(x) in [a,b]}
+If f (x) ≥ g (x) in [a, c] and f (x) ≤ g (x) in [c, b], a < c < b, then the resultant area between the curve is given as,
+
+Area=∫[f(x)−g(x)].dx, + ∫[g(x)−f(x)].dx {limits from a to c → a to b + b to c}
+
+Probability:
+The possibility of an event or outcome occurring dependent on the occurrence of a preceding event or outcome is known as conditional probability. It simply depends on any previous occurrence that has already occurred. Consider two events A and B with the same sample space of a random experiment, then the conditional probability of the event A gives that B has occurred, i.e. P(A|B) is,
+P(A|B) = P(A ∩ B)/P(B)
+
+(for P(B) ≠ 0).
+
+When E and F are the events of a sample space S of an experiment: P(S|F) = P(F|F) = 1
+When A and B are any two events in a sample space S and F an event of S, such that P(F)≠0: P((A ∪ B)|F) = P(A|F) + P(B|F) – P((A ∩ B)|F)
+P(E′|F) = 1 − P(E|F)
+Multiplication Rule: Consider two events such as, E and F from a sample space S. Here, the set E ∩ F denotes the event that both E and F have occurred. Or we can say, E ∩ F represents the simultaneous occurrence of the events E and F. The event E ∩ F is also written as EF. According to this rule, if E and F are the events in a sample space, then;
+
+P(E ∩ F) = P(E) P(F|E) = P(F) P(E|F)
+
+where P(E) ≠ 0 and P(F) ≠ 0.
+
+Similarly, for three events E, F, and G from a sample S:
+
+P(E ∩ F ∩ G) = P(E) P(F|E) P(G|(E ∩ F)) = P(E) P(F|E) P(G|EF)  
+
+Independent Events: Two experiments are said to be independent if the probability of the events E and F occurring simultaneously when the two experiments are performed is the product of P(E) and P(F) calculated separately on the basis of two experiments, i.e., for every pair of events E and F, where E is associated with the first experiment and F with the second experiment.
+
+P (E ∩ F) = P (E).P(F)
+
+Baye’s Theorem: A set of events E1, E2, …, En is said to denote a partition of the sample space S when,
+
+Ei ∩ Ej = φ, i ≠ j, i, j = 1, 2, 3, …, n
+E1 ∪ Ε2 ∪ … ∪ En = S and
+P(Ei )> 0 for all i = 1, 2, …, n.
+
+Theorem of Total Probability: Let E1, E2, …., En be the partition of a sample space and A be any event; then,
+
+P(A) = P(E1) P (A|E1) + P (E2) P (A|E2) + … + P (En) . P(A|En)
 
 `;
 
